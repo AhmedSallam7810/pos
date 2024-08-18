83c4d6d09e3638e40b870e571d82d48331722b2b
Ahmed Sallam <76839975+AhmedSallam7810@users.noreply.github.com>
2024-08-18 13:49:23 +0300

    Add files via upload

---
+++ lang/en/site.php
@@ -0,0 +1,88 @@
+<?php
+
+
+
+return [
+
+  'dashboard'=>'dashboard',
+  'users'=>'users',
+  'first_name'=>'first name',
+  'last_name'=>'lasr name',
+  'email'=>'email',
+  'password'=>'password',
+  'image'=>'image',
+  'action'=>'action',
+  'search'=>'search',
+  'add'=>'add',
+  'delete'=>'delete',
+
+    'logout' => 'logout',
+
+    'create' => 'create',
+    'read' => 'read',
+    'edit' => 'edit',
+    'update' => 'update',
+    'delete' => 'delete',
+    'show' => 'show',
+    'loading' => 'loading',
+    'print' => 'print',
+
+    'confirm_delete' => 'confirm_delete',
+    'yes' => 'yes',
+    'no' => 'no',
+
+    'login' => 'no',
+    'remember_me' => 'remember_me',
+    'password' => 'password ',
+    'password_confirmation' => 'password_confirmation',
+
+    'added_successfully' => 'added_successfully',
+    'updated_successfully' => 'updated_successfully',
+    'deleted_successfully' => 'deleted_successfully',
+
+    'no_data_found' => 'no data found',
+    'no_records' => 'no records',
+
+    'clients' => 'clients',
+    'client_name' => 'client name',
+    'phone' => 'phone',
+    'address' => 'address',
+    'previous_orders' => 'previous orders',
+    'orders' => 'orders',
+    'add_order' => 'add order',
+    'edit_order' => 'edit_order',
+
+    'permissions' => 'permissions',
+
+    'categories' => 'categories',
+    'all_categories' => 'all_categories' ,
+    'name' => 'name',
+    'description' => 'description',
+    'products_count' => 'products_count',
+    'related_products' => 'related_products',
+    'category' => 'category',
+    'show_products' => 'show_products',
+    'created_at' => 'created_at',
+
+    'products' => 'products',
+    'product' => 'product',
+    'quantity' => 'quantity',
+    'total' =>'total',
+    'purchase_price' => 'purchase_price',
+    'price' =>'price' ,
+    'sale_price' => 'sale_price',
+    'stock' => 'stock',
+    'profit_percent' => 'profit_percent',
+
+    'ar' => [
+        'name' =>'arabic name',
+        'description' =>'arabic description',
+    ],
+
+    'en' => [
+        'name' => 'english name',
+        'description' => 'english description',
+    ],
+
+  
+];
